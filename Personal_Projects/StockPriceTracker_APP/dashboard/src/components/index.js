f8faf836032416d81fde2ab4a2b03586d6e17da4
JanPotat0 <jancquimpo@gmail.com>
2026-05-20 12:06:43 -0400

    minor changes made and fixed

--- Personal_Projects/StockPriceTracker_APP/dashboard/src/components/index.js
+++
@@ -1,6 +0,0 @@
-// src/components/index.js
-// Central export point for all components
-
-export { default as TopMovers } from './TopMovers';
-export { default as OHLCData } from './OHLCData';
-export { default as WatchlistMonitor } from './WatchlistMonitor';
